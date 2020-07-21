52132c852beba83dbaa58ed794e142b4b6f60e76
anderson <anderdavid86@hotmail.com>
2020-07-21 18:40:24 -0500

    pruebas base de datos en produccion

---
+++ db/database.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql')
+
+/* const params ={
+    host:'localhost',
+    user:'root',
+    password:root,
+    database:'asignacion'
+} */
+
+const params ={
+    host:'sql10.freemysqlhosting.net',
+    user:'sql10356362',
+    password:'sGNCHQAkQA',
+    database:'sql10356362'
+}
+
+/* Server: sql10.freemysqlhosting.net
+		Name: sql10356362
+		Username: sql10356362
+		Password: sGNCHQAkQA
+		Port number: 3306 */
--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express')
 const app = express()
+const pool = require('../db/database')  
+const { response } = require('express')
+
 const port = process.env.PORT||3002
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port port!`))
+
+app.get('/estudiantes',(req,res)=>{
+    let query ="SELECT *FROM estudiantes"
+
+    pool.query(query,(err,result,fields)=>{
+        if(err) throw err
+
+        if(result.length>0){
+            response ={
+                status:true,
+                estudiantes:result
+            }
+        }else{
+            response={
+                status:false,
+                msg:'No hay estudiantes',
+                estudiantes:result
+            }
+        }
+    })
+})
